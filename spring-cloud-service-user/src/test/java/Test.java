61ecb5c85d141d2e4c90c2f4ebff764fbb0fb76b
zhangjinfeng <zhangjinfeng@eagle-realestate.com>
2019-02-20 13:07:59 +0800

    集群

--- spring-cloud-service-user/src/test/java/Test.java
+++ spring-cloud-service-user/src/test/java/Test.java
@@ -1,21 +1,42 @@
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.commons.util.InetUtilsProperties;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by paul on 2019/2/16.
  */
 public class Test {
     public static void main(String[] args) {
-        while (true){
-            InetUtils inetUtils = new InetUtils(new InetUtilsProperties());
-            String hostname = inetUtils.findFirstNonLoopbackHostInfo().getHostname();
-            System.out.println(hostname);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1, 2, 0, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>()
+        );
+        Future<?> submit = threadPoolExecutor.submit(() -> {
             try {
-                Thread.currentThread().sleep(1000);
-            } catch (InterruptedException e) {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+        });
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        submit.cancel(true);
+
+
+//        while (true){
+//            InetUtils inetUtils = new InetUtils(new InetUtilsProperties());
+//            String hostname = inetUtils.findFirstNonLoopbackHostInfo().getHostname();
+//            System.out.println(hostname);
+//            try {
+//                Thread.currentThread().sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
 
 
